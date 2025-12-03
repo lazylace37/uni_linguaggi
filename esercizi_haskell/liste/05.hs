b4ed851de77728e5c460d98705be242d51776293
Gioele Vuaran <vuarangioele@gmail.com>
2025-12-03 22:37:41 +0100

    feat: update liste 05.hs

--- esercizi_haskell/liste/05.hs
+++ esercizi_haskell/liste/05.hs
@@ -5,3 +5,9 @@ sumList (x:xs) = x + sumList xs
 calc :: [Integer] -> [(Integer, Integer)]
 calc [] = []
 calc (x:xs) = (x, sumList xs) : calc xs
+
+-- oppure con fold
+calc1 [] = []
+calc1 xs = fst (foldr step ([], 0) xs)
+  where
+    step x (ys, s) = ((x, s) : ys, s + x)
